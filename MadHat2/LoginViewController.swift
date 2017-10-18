10a58d2c9819f61a83e21ee283d92acb3c49cfea
prashanth <Prashanth.Kothapalli@kaspersky.com>
2017-10-18 14:24:38 -0500

    updated login view

--- MadHat2/LoginViewController.swift
+++ MadHat2/LoginViewController.swift
@@ -10,36 +10,83 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    
     @IBOutlet weak var UserName: UITextField!
     
     @IBOutlet weak var UserPassword: UITextField!
-
-    @IBAction func UserLogin(_ sender: Any) {
+    
+    @IBOutlet weak var LoginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        let Name = UserName.text
-        let Password = UserPassword.text
+        UserName.layer.borderWidth = 1
+        UserName.layer.borderColor = UIColor.black.cgColor
+        UserName.layer.cornerRadius = 20
+        UserName.clipsToBounds = true
         
-        let soapAPI = String(format:
-            "<?xml version='1.0' encoding='utf-8'?><s:Envelope xmlns:s='http://schemas.xmlsoap.org/soap/envelope/'><s:Body><DoLogin xmlns='http://madhattvguide.azurewebsites.net' xmlns:i='http://www.w3.org/2001/XMLSchema-instance'><UserName>%@</UserName><Password>%@</Password><OS>Win</OS><AppVersion>3.5.3.0</AppVersion><OrgID>98975336-99DC-4C81-A3C7-067E76AD0BD5</OrgID></DoLogin></s:Body></s:Envelope>", Name!, Password!
-        )
+        UserPassword.layer.borderWidth = 1
+        UserPassword.layer.borderColor = UIColor.black.cgColor
+        UserPassword.layer.cornerRadius = 20
+        UserPassword.clipsToBounds = true
         
-        let urlString = "https://service.madhat.tv/TVGuideService.asmx"
-        let url = URL(string: urlString)
-        
-        var theRequest = URLRequest(url: url! as URL);
-        
-        let msgLength = soapAPI.count;
-        
-        theRequest.addValue("text/xml; charset=utf-8", forHTTPHeaderField: "Content-Type")
-        theRequest.addValue(String(msgLength), forHTTPHeaderField: "Content-Length")
-        theRequest.httpMethod = "POST"
-        theRequest.httpBody = soapAPI.data(using: String.Encoding.utf8, allowLossyConversion: false)
-        let connection = URLSession(configuration: theRequest)
-        connection!.start()
-        
-        if(connection == true) {
-            var mutableData : Void = NSMutableData.initialize()
-        }
-
+        LoginButton.layer.cornerRadius = 20
+        LoginButton.clipsToBounds = true
+    
     }
+    
+//    @IBAction func UserLogin(_ sender: Any) {
+//
+//        let Name = UserName.text
+//        let Password = UserPassword.text
+//
+//        let soapAPI = String(format:
+//            "<?xml version='1.0' encoding='utf-8'?><s:Envelope xmlns:s='http://schemas.xmlsoap.org/soap/envelope/'><s:Body><DoLogin xmlns='http://madhattvguide.azurewebsites.net' xmlns:i='http://www.w3.org/2001/XMLSchema-instance'><UserName>%@</UserName><Password>%@</Password><OS>Win</OS><AppVersion>3.5.3.0</AppVersion><OrgID>98975336-99DC-4C81-A3C7-067E76AD0BD5</OrgID></DoLogin></s:Body></s:Envelope>", Name!, Password!
+//        )
+//
+//        let urlString = "https://service.madhat.tv/TVGuideService.asmx"
+//        let url = NSURL(string: urlString)
+//
+//        var theRequest = NSMutableURLRequest(url: url! as URL);
+//
+//        let msgLength = String(describing: soapAPI.characters.count)
+//
+//        theRequest.addValue("text/xml; charset=utf-8", forHTTPHeaderField: "Content-Type")
+//        theRequest.addValue(String(msgLength), forHTTPHeaderField: "Content-Length")
+//        theRequest.httpMethod = "POST"
+//        theRequest.httpBody = soapAPI.data(using: String.Encoding.utf8, allowLossyConversion: false)
+//
+//        let session = URLSession.shared
+//
+//        let task =  session.dataTask(with: theRequest) { (data, resp, error) in
+//
+//            guard error == nil && data != nil else{
+//
+//                print("connection error or data is nill")
+//
+//                return
+//            }
+//
+//            if resp != nil {
+//
+//                self.mutableData.length = 0;
+//
+//            }
+//
+//            let mutableData : Void = NSMutableData.initialize()
+//            print(mutableData)
+//
+//            let dataString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+//            self.mutableData.append(data!)
+//            print(dataString)
+//
+//            let xmlParser = XMLParser(data: mutableData as Data)
+//            xmlParser.delegate = self
+//            xmlParser.parse()
+//            xmlParser.shouldResolveExternalEntities = true
+//
+//        }
+//        task.resume()
+//
+//    }
 }
